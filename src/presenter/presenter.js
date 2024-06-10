55ca5116f50be7107f63755ef4dafd78fba7ab12
Romaronio <spirinr800@gmail.com>
2024-06-10 23:31:07 +0300

    presenter

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -1,15 +1,19 @@
 import { render } from '../framework/render.js';
-import DrawPoint from '../view/draw_point.js';
+import { updateDate } from '../utils/is-favorite.js';
 import Filter from '../view/Filter.js';
+import DrawPoint from '../view/draw_point.js';
 import TripPresenter from './trip-presenter.js';
 export default class Presenter {
   boardComponent = new DrawPoint();
+  pointMap = new Map();
+  points = [];
   constructor({container, pointModel}) {
     this.container = container;
     this.pointModel = pointModel;
   }
 
   init() {
+    this.ponts = this.pointModel.getPoints();
     this.#renderBoardComponent();
     this.#renderFilter();
     this.#renderTrip();
@@ -24,10 +28,26 @@ export default class Presenter {
   }
 
   #renderTrip() {
-    const TripPres = new TripPresenter(this.container, this.pointModel);
-    TripPres.init();
+    for(const point of this.pointModel.getPoints()) {
+      const tripPres = new TripPresenter(
+        {
+          container: this.container,
+          pointModel: this.pointModel,
+          point: point,
+          pointUpdate: this.#pointUpdate,
+          onEditClose: this.onEditClose
+        });
+      tripPres.init();
+      this.pointMap.set(point.id, tripPres);
+    }
   }
 
+  #pointUpdate = (updateItem) => {
+    this.points = updateDate(this.points, updateItem);
+    this.pointMap.get(updateItem.id).init(updateItem);
+  };
+
+  onEditClose = () => {
+    this.pointMap.forEach((presenter) => presenter.setView());
+  };
 }
-
-
